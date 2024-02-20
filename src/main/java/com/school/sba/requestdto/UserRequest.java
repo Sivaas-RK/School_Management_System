e00e8ff70ed8eff7c91c0284fd14e4ed6cf4a76d
Sivaas-RK <sivugit@gmail.com>
2024-02-20 21:58:03 +0530

    worked on user implementation

---
+++ src/main/java/com/school/sba/requestdto/UserRequest.java
@@ -0,0 +1,26 @@
+package com.school.sba.requestdto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest {
+
+
+	private String userName;
+	private String userPass;
+	private String userFirstName;
+	private String userLastName;
+	
+	private long userContact;
+	
+	private String userEmail;
+	private UserRole userRole;
+}
---
+++ src/main/java/com/school/sba/service/IUserService.java
@@ -0,0 +1,30 @@
+package com.school.sba.service;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.school.sba.util.ResponseStructure;
+
+public interface lUserService {
+	
+	public ResponseEntity<ResponseStructure<UserResponse>> registerAdmin(UserRequest userRequest);
+	
+	public ResponseEntity<ResponseStructure<UserResponse>> addOtherUser(UserRequest userRequest);
+
+
+	public ResponseEntity<ResponseStructure<UserResponse>> findUser(Integer userId);
+
+	public ResponseEntity<ResponseStructure<UserResponse>> softDeleteUser(Integer userId);
+	
+	public ResponseEntity<ResponseStructure<UserResponse>> updateUser(int userId, UserRequest userRequest);
+
+	public ResponseEntity<ResponseStructure<UserResponse>> assignSubjectToTeacher(int subjectId, int userId);
+
+	public ResponseEntity<ResponseStructure<UserResponse>> assignUserToProgram(Integer programId, Integer userId);
+	
+	public ResponseEntity<ResponseStructure<List<UserResponse>>> getUsersByRoleAndAcademicProgram(int programId,
+			String userRole);
+ 
+
+}
