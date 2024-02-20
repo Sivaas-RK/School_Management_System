93587beaae379441150187c8e2c87f7450faf885
Sivaas-RK <sivugit@gmail.com>
2024-02-20 20:58:20 +0530

    Project created

---
+++ src/main/java/com/school/sba/entity/Schedule.java
@@ -0,0 +1,36 @@
+package com.school.sba.entity;
+
+import java.time.LocalTime;
+
+import ch.qos.logback.core.util.Duration;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Schedule {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer scheduleId;
+	private LocalTime opensAt;
+	private LocalTime closesAt;
+	private Integer classHoursPerDay;
+	private Duration classHoursLengthInMin;
+	private LocalTime breakTime;
+	private Duration breakLengthInMin;
+	private LocalTime lunchTime;
+	private Duration lunchLengthInMin;
+	
+}
---
+++ src/main/java/com/school/sba/entity/School.java
@@ -0,0 +1,44 @@
+package com.school.sba.entity;
+
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class School {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer schoolId;
+	private String schoolName;
+	private Long schoolContactNumber;
+	private String schoolEmailId;
+	private String schoolAddress;
+	private boolean isDeleted;
+	
+	@OneToOne
+	private Schedule schedule;
+	
+	@OneToMany(mappedBy="school",cascade = CascadeType.REMOVE)
+	private List<AcademicProgram> listOfAcademicPrograms;
+	
+	
+}
