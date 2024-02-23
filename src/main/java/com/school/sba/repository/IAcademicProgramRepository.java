4936bd1e9a92643feeff3b962f46edfce951f586
Sivaas-RK <sivugit@gmail.com>
2024-02-23 12:25:51 +0530

    worked on repository implementation

---
+++ src/main/java/com/school/sba/repository/IAcademicProgramRepository.java
@@ -0,0 +1,13 @@
+package com.school.sba.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.school.sba.entity.AcademicProgram;
+
+public interface IAcademicProgramRepository extends JpaRepository<AcademicProgram, Integer>{
+	
+	List<AcademicProgram> findByIsDeletedIsTrue();
+	
+}
